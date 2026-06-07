c2ffa7701132ef42f25f73b7b6bfbf574dc84159
Malrik The Mad <VorpalGnome@gmail.com>
2026-06-07 16:20:31 -0700

    CodeFixesandcleanup

---
+++ js/menu-return.js
@@ -0,0 +1,9 @@
+﻿const RETURN_TO_GAME_STORAGE_KEY = "lab-zero-return-to-game";
+
+function markReturnToGame() {
+  sessionStorage.setItem(RETURN_TO_GAME_STORAGE_KEY, "true");
+}
+
+document.querySelectorAll("[data-return-to-game]").forEach((link) => {
+  link.addEventListener("click", markReturnToGame);
+});

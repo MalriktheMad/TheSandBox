b5577ee77a5f1862cae95d50c6be3615edeaf0d6
Kevin Klinkert <VorpalGnome@gmail.com>
2026-06-07 17:33:59 -0700

    Update dialogue.js

--- js/dialogue.js
+++ js/dialogue.js
@@ -203,9 +203,9 @@ function getCodexDialogueLines() {
 
 function getLizDialogueLines() {
   return [
-    lizLine("Hold still a second, Little Wing. I am trying to remember the shape of this light."),
-    littleWingLine("I am very still. Extremely still."),
-    lizLine("You are a blur with feathers, but I like the energy.")
+    lizLine("Little-wing! You came to see us! Do try not to get paint on your feathers."),
+    littleWingLine("I'll be careful or else the wing-master will preen me again."),
+    lizLine("You should go check the kitchen i'm sure there are some snacks.")
   ];
 }
 function getOldDillyDialogueLines() {
